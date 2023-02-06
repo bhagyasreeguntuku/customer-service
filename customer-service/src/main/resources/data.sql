b462bccac09436f03cc4b6199f9d71d736c9207b
bhagyasreeguntuku <bhagyasreeguntuku@gmail.com>
2023-02-06 13:29:51 +0530

    adding application.properties and data.sql

---
+++ customer-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (id, active,name) VALUES (1,true,'Bhagya');
+INSERT INTO customer (id, active, name) VALUES (2,true,'Arangi');
+INSERT INTO customer (id, active, name) VALUES (3,false,'Arpita');
+INSERT INTO customer (id, active, name) VALUES (4,false,'Supritha');
+INSERT INTO customer (id, active, name) VALUES (5,true,'Noopur');
